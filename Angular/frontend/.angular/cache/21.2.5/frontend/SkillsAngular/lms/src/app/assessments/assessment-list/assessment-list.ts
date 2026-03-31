d0461b5e59fba1b4320f9e988bee7149778b6e86
Siphamandlagift <siphamandlagift730@gmail.com>
2026-03-31 20:08:35 +0200

    sd

---
+++ Angular/frontend/.angular/cache/21.2.5/frontend/SkillsAngular/lms/src/app/assessments/assessment-list/assessment-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assessment-list',
+  imports: [],
+  templateUrl: './assessment-list.html',
+  styleUrl: './assessment-list.css',
+})
+export class AssessmentList {}
---
+++ Angular/frontend/.angular/cache/21.2.5/frontend/SkillsAngular/lms/src/app/assignments/assignment-detail/assignment-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-detail',
+  imports: [],
+  templateUrl: './assignment-detail.html',
+  styleUrl: './assignment-detail.css',
+})
+export class AssignmentDetail {}
